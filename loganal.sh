e9804fcaf078f16c3fda61a4c3dff9e9268a818e
Franck Ridel <68272257+cryptrz@users.noreply.github.com>
2025-01-08 12:42:39 +0100

    Update loganal.sh
    Added journalctl details for 1 day, 1 week and 1 month.

--- loganal.sh
+++ loganal.sh
@@ -20,19 +20,37 @@ ERROR_COUNT=$(grep -ci 'ERROR' "$LOG_FILE")
 WARNING_COUNT=$(grep -ci 'WARNING' "$LOG_FILE")
 CRITICAL_EVENTS=$(grep -ni 'CRITICAL' "$LOG_FILE")
 
+ERROR_COUNT_YESTERDAY=$(journalctl -p err --since=yesterday | wc -l)
+ERROR_COUNT_WEEK=$(journalctl -p err --since="1 week ago" | wc -l)
+ERROR_COUNT_MONTH=$(journalctl -p err --since="1 month ago" | wc -l)
+
+CRIT_COUNT_YESTERDAY=$(journalctl -p crit --since=yesterday | wc -l)
+CRIT_COUNT_WEEK=$(journalctl -p crit --since="1 week ago" | wc -l)
+CRIT_EVENTS_YESTERDAY=$(journalctl -p crit --since=yesterday)
+CRIT_EVENTS_WEEK=$(journalctl -p crit --since="1 week ago")
+CRIT_EVENTS_MONTH=$(journalctl -p crit --since="1 month ago")
 # Generate the report
 {
     echo "Log Analysis Report - $(date)"
     echo "Analyzed Log File: $LOG_FILE"
-    echo "Total Error Count: $ERROR_COUNT"
-    echo "Total Warning Count: $WARNING_COUNT"
-    echo -e "\nCritical Events (line numbers):"
+    echo -e "\n$(basename $1) Total Error Count: $ERROR_COUNT"
+    echo "$(basename $1) Total Warning Count: $WARNING_COUNT"
+    echo -e "\n$(basename $1) Critical Events (line numbers):"
 
-    if [ -z "$CRITICAL_EVENTS" ]; then
+    if [ -z "$CRITICAL_EVENTS" ] && [ -z "$CRIT_COUNT_YESTERDAY" ] && [ "$CRIT_COUNT_WEEK" ]; then
         echo "No critical events found."
     else
-        echo "$CRITICAL_EVENTS"
+	echo "$CRITICAL_EVENTS"
     fi
+
+    echo -e "***************\nJOURNALCTL LOGS\n***************"
+    echo "Error Count since yesterday: $ERROR_COUNT_YESTERDAY"
+    echo "Error Count since 1 week: $ERROR_COUNT_WEEK"
+    echo "Error Count since 1 month: $ERROR_COUNT_MONTH"
+    echo -e "\nCritical events since yesterday: \n$CRIT_EVENTS_YESTERDAY"
+    echo -e "\nCritical events since 1 week: \n$CRIT_EVENTS_WEEK"
+    echo -e "\nCritical events since 1 month: \n$CRIT_EVENTS_MONTH"
+    
 } > "$REPORT_FILE"
 
 echo "Analysis complete. Report saved to '$(pwd)/$REPORT_FILE'."
